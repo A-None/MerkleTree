785659a7aaa918eb3634df09f735c6ef68be896e
A-None <floppynarwhal@gmail.com>
2021-06-18 11:30:48 -0400

    initial: Merkle Tree Airdrop

---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,8 @@
+import { ethers } from 'ethers';
+
+export const distributorAddress = '0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690';
+export const distributeTokenAddress = '0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E';
+
+export const user1 = ['0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266' + " " + ethers.utils.parseEther("5000"), "5000"];
+export const user2 = ['0x70997970c51812dc3a010c7d01b50e0d17dc79c8' + " " + ethers.utils.parseEther("10000"), "10000"];
+export const user3 = ['0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc' + " " +  ethers.utils.parseEther("100"), "100"];
